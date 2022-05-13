20f80da733dfe8e52a135e7689b67b16b9b31d60
gev02 <glendi95st21@gmail.com>
2022-05-13 12:48:09 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+
+app.post('/alta-usuario', ( req, res ) => {
+  console.log('usuario', req.body)
+  res.send(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
